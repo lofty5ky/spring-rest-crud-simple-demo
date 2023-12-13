9905620563825a5fd24d828a8941bc5a4cfbe6c7
loftysky <lieuht2040@gmail.com>
2023-12-13 21:45:49 +0700

    Initial commit

---
+++ src/main/java/org/example/springrestcrudsimpledemo/SpringRestCrudSimpleDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.springrestcrudsimpledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestCrudSimpleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestCrudSimpleDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springrestcrudsimpledemo/SpringRestCrudSimpleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springrestcrudsimpledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestCrudSimpleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
